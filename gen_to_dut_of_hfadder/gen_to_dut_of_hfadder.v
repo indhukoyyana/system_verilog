4000e17224fab4f6df03b1b839467b111dc8b86b
Indhu koyyana <140484677+indhukoyyana@users.noreply.github.com>
2025-08-09 19:46:29 +0530

    Create gen_to_dut_of_hfadder.v

---
+++ gen_to_dut_of_hfadder/gen_to_dut_of_hfadder.v
@@ -0,0 +1,13 @@
+module hf(a,b,sum,carry);
+  input a,b;
+  output sum,carry;
+  assign sum=a^b;
+  assign carry=a&b;
+endmodule
+
+interface inter;
+  logic a;
+  logic b;
+  logic sum;
+  logic carry;
+endinterface
